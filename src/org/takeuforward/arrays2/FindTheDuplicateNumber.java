d8eed954639ff055597bd359a15d42f505d5bf09
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-29 02:57:57 +0530

    feature: FindTheDuplicateNumber.java

---
+++ src/org/takeuforward/arrays2/FindTheDuplicateNumber.java
@@ -0,0 +1,26 @@
+package org.takeuforward.arrays2;
+
+import java.util.Arrays;
+
+public class FindTheDuplicateNumber {
+    static class Solution {
+        public int findDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            int result = -1;
+            for (int i=0; i<nums.length; i++) {
+                if (i+1<nums.length && nums[i+1] == nums[i]) {
+                    result = nums[i];
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        int[] nums = {1,3,4,2,2}; // 2
+        int result = sl.findDuplicate(nums);
+        System.out.println(result);
+    }
+}
