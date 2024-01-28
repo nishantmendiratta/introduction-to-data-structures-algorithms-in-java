04f9c4451a894118e005a0da5c91b466f3eb65c0
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-29 02:10:14 +0530

    feature: MergeIntervals.java

---
+++ src/org/takeuforward/arrays2/MergeIntervals.java
@@ -0,0 +1,45 @@
+package org.takeuforward.arrays2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MergeIntervals {
+    static class Solution {
+        public int[][] merge(int[][] intervals) {
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+            ArrayList<int[]> result = new ArrayList<>();
+            for (int i=0; i<intervals.length;i++) {
+                if (i+1 < intervals.length && intervals[i+1][0]<=intervals[i][1]) {
+                    intervals[i+1][0] = Math.min(intervals[i][0], intervals[i+1][0]);
+                    intervals[i+1][1] = Math.max(intervals[i+1][1], intervals[i+1][1]);
+                } else {
+                    result.add(intervals[i]);
+                }
+            }
+            int[][] requestedOutput = new int[result.size()][2];
+            for (int i=0; i<result.size(); i++) {
+                requestedOutput[i] = result.get(i);
+            }
+            return requestedOutput;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+//        int[][] intervals = {
+//                {1,3},
+//                {2,6},
+//                {8,10},
+//                {15,18}
+//        };
+        int[][] intervals = {
+                {1,4},
+                {0,4},
+        };
+        int[][] result = sl.merge(intervals);
+        for (int i=0;i<result.length;i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+}
