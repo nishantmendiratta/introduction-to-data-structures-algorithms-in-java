221fde9ba53563f080aec6377792386ba756ca42
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-29 17:59:36 +0530

    feature: MergeSortedArray.java -- optimized

--- src/org/takeuforward/arrays2/MergeSortedArray.java
+++ src/org/takeuforward/arrays2/MergeSortedArray.java
@@ -5,32 +5,49 @@ import java.util.*;
 // https://leetcode.com/problems/merge-sorted-array/submissions/1159536336/
 public class MergeSortedArray {
     static class Solution {
+        // Three pointers approach - https://leetcode.com/problems/merge-sorted-array/editorial/
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            int p1 = m-1;
-            int p2 = n-1;
-            for (int p=m+n-1; p>=0; p--) {
-                if (p2<0) {
+            int rightPointer1 = m - 1;
+            int rightPointer2 = n - 1;
+            for (int writePointer = m + n - 1; writePointer >= 0; writePointer--) {
+                if (rightPointer1 >= 0 && rightPointer2 >= 0) {
+                    nums1[writePointer] = nums1[rightPointer1] > nums2[rightPointer2] ? nums1[rightPointer1--] : nums2[rightPointer2--];
+                } else if (rightPointer1 >= 0) {
+                    nums1[writePointer] = nums1[rightPointer1--];
+                } else {
+                    nums1[writePointer] = nums2[rightPointer2--];
+                }
+            }
+        }
+
+        public void mergeX(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = m - 1;
+            int p2 = n - 1;
+            for (int p = m + n - 1; p >= 0; p--) {
+                if (p2 < 0) {
                     break;
                 }
-                if (p1>=0 && nums1[p1] > nums2[p2]) {
+                if (p1 >= 0 && nums1[p1] > nums2[p2]) {
                     nums1[p] = nums1[p1--];
                 } else {
                     nums1[p] = nums2[p2--];
                 }
             }
         }
+
         public void mergeBruteForce(int[] nums1, int m, int[] nums2, int n) {
-            for(int i=0; i<n;i++) {
-                nums1[i+m] = nums2[i];
+            for (int i = 0; i < n; i++) {
+                nums1[i + m] = nums2[i];
             }
             Arrays.sort(nums1);
         }
+
         public void mergeBruteForce2(int[] nums1, int m, int[] nums2, int n) {
             int left = 0;
             int right = 0;
             ArrayList<Integer> result = new ArrayList<>();
 
-            while (left < m && right<n) {
+            while (left < m && right < n) {
                 if (nums1[left] < nums2[right]) {
                     result.add(nums1[left++]);
                 } else {
@@ -38,14 +55,14 @@ public class MergeSortedArray {
                 }
             }
 
-            while (left<m) {
+            while (left < m) {
                 result.add(nums1[left++]);
             }
-            while (right<n) {
+            while (right < n) {
                 result.add(nums2[right++]);
             }
-            int i=0;
-            for (int it: result) {
+            int i = 0;
+            for (int it : result) {
                 nums1[i] = it;
                 i++;
             }
