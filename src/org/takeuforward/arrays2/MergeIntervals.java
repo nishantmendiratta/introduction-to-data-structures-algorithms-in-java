e35881521b98f93dd682372758f915b225d7991a
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-29 15:21:37 +0530

    feature: MergeIntervals.java -- optimized

--- src/org/takeuforward/arrays2/MergeIntervals.java
+++ src/org/takeuforward/arrays2/MergeIntervals.java
@@ -7,21 +7,18 @@ import java.util.Arrays;
 public class MergeIntervals {
     static class Solution {
         public int[][] merge(int[][] intervals) {
-            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
             ArrayList<int[]> result = new ArrayList<>();
-            for (int i=0; i<intervals.length;i++) {
-                if (i+1 < intervals.length && intervals[i+1][0]<=intervals[i][1]) {
-                    intervals[i+1][0] = Math.min(intervals[i][0], intervals[i+1][0]);
-                    intervals[i+1][1] = Math.max(intervals[i+1][1], intervals[i+1][1]);
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+            for (int i = 0; i < intervals.length; i++) {
+                if (i + 1 < intervals.length && intervals[i + 1][0] <= intervals[i][1]) {
+                    intervals[i + 1][0] = Math.min(intervals[i][0], intervals[i + 1][0]);
+                    intervals[i + 1][1] = Math.max(intervals[i+1][1], intervals[i][1]);
                 } else {
                     result.add(intervals[i]);
                 }
             }
-            int[][] requestedOutput = new int[result.size()][2];
-            for (int i=0; i<result.size(); i++) {
-                requestedOutput[i] = result.get(i);
-            }
-            return requestedOutput;
+
+            return result.toArray(new int[result.size()][]);
         }
     }
 
