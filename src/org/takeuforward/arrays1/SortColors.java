f12d6aa7cdd964f9f394b4290dd28bdbb0bfbc0a
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-29 15:10:23 +0530

    feature: SortColors.java -- optimized

--- src/org/takeuforward/arrays1/SortColors.java
+++ src/org/takeuforward/arrays1/SortColors.java
@@ -5,7 +5,28 @@ import java.util.Arrays;
 // https://leetcode.com/problems/sort-colors/
 public class SortColors {
     static class Solution {
+        public void swap(int[] arr, int a, int b) {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
         public void sortColors(int[] nums) {
+            int low =0, high = nums.length-1;
+            int mid = 0;
+            while (mid <= high) {
+                if (nums[mid] ==0) {
+                    swap(nums, mid, low);
+                    mid++;
+                    low++;
+                } else if (nums[mid] == 1) {
+                    mid++;
+                } else {
+                    swap(nums, mid, high);
+                    high--;
+                }
+            }
+        }
+        public void sortColorsBruteForce(int[] nums) {
             int onesCount = 0, zerosCount = 0;
             for (int i=0; i<nums.length; i++) {
                 if (nums[i] == 0) {
---
+++ src/org/takeuforward/arrays3/MajorityElementII.java
@@ -0,0 +1,2 @@
+package org.takeuforward.arrays3;public class MajorityElementII {
+}
