87c8da8e9778f24db9e3c4c16148602e5a80761f
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2024-01-31 09:03:19 +0530

    feature: /MajorityElement.java -- optimized using Moore's voting algorithm

--- src/org/takeuforward/arrays3/MajorityElement.java
+++ src/org/takeuforward/arrays3/MajorityElement.java
@@ -5,7 +5,34 @@ import java.util.*;
 // https://leetcode.com/problems/majority-element/
 public class MajorityElement {
     static class Solution {
+        // Moore's voting algorithm
         public int majorityElement(int[] nums) {
+            int count = 0;
+            int element = 0;
+            for (int num: nums) {
+                if (count == 0) {
+                    count = 1;
+                    element = num;
+                } else if (num == element) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+
+            //checking if the stored element is the majority element:
+            int result = 0;
+            for (int num: nums) {
+                if (num == element) {
+                    result++;
+                }
+            }
+            if (result>nums.length/2) {
+                return element;
+            }
+            return -1;
+        }
+        public int majorityElementBetterSolution(int[] nums) {
             int result = -1;
             Map<Integer, Integer> hm = new HashMap<>();
             for (int i=0; i<nums.length; i++) {
